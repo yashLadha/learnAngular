90ed9743541556ac017e1a0076ccdbc895ce88ee
yashLadha <201551061@iiitvadodara.ac.in>
2017-10-19 15:50:08 +0530

    Template form part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MdButtonModule, MdCheckboxModule, MdToolbarModule, MdListModule, MdLineModule, MdGridListModule,
-  MdCardModule } from '@angular/material';
+  MdCardModule, MdDialogModule, MdInputModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
@@ -16,6 +16,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
@@ -32,7 +33,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -40,16 +42,21 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FormsModule,
     HttpModule,
     MdButtonModule,
+    MdInputModule,
     MdListModule,
     MdCheckboxModule,
     MdGridListModule,
     MdLineModule,
     MdCardModule,
+    MdDialogModule,
     MdToolbarModule,
     FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [ DishService, PromotionService, LeaderService ],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
